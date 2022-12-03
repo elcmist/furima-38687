c43f534273368949032e1a1b01f9aef48e3bcebd
elcmist <mist303s@gmail.com>
2022-12-03 21:19:04 +0900

    新規登録・ログイン・ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birthday])
   end
 
 end
