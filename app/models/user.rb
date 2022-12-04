1b44d5715a27f4c579a1c97a06f3f3f5f6983e1d
elcmist <mist303s@gmail.com>
2022-12-04 14:09:35 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,13 @@ class User < ApplicationRecord
 
          with_options presence: true do
           validates :nickname
-          validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i }
-          validates :password, format: { with: /\A[a-z0-9]+\z/i }
+          
           validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
           validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
           validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/ }
           validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }
           validates :birthday
          end 
+         validates :password, format: { with: /\A[a-z0-9]+\z/i }
+
 end
