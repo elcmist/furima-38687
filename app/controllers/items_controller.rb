d72dd6151d8c0a2ef0d27efc6aac487695510770
elcmist <mist303s@gmail.com>
2022-12-06 19:53:05 +0900

    未ログインでの出品機能の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index ]
+  
+  
   def index
 
   end
@@ -6,4 +9,35 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def create
+    
+    @item = Item.new(create_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+
+  private
+   def create_params
+    params.required(:item).permit(:image, :name, :explanation, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+   end
+
+   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+
+ 
+
+
+
 end
+
