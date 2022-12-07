cbc1cfa1cc53b8b270897435432f7c3106a57442
elcmist <mist303s@gmail.com>
2022-12-07 17:36:42 +0900

    テストコード記述

---
+++ app/javascript/new.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', () => {
+  
+
+// 金額を入力した数値をpriceInputという変数に格納する
+const inputPrice = document.getElementById("item-price");
+const content = document.getElementById("add-tax-price")
+const profit = document.getElementById("profit")
+  
+  inputPrice.addEventListener('input', function(){
+    const price = document.getElementById("item-price").value;
+    tax_price = Math.floor(price/10);
+    content.innerHTML = tax_price
+    
+    Net_income = (price - tax_price);
+    profit.innerHTML = Net_income
+    console.log(profit)
+  })
+})
